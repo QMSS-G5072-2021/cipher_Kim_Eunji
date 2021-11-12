df1e9dd9b34bff339030d582ba6ec9d0b7a9a639
Eunji Kim <ekim1396@gmail.com>
2021-11-12 08:59:41 -0500

    adding test for cipher function

--- tests/test_cipher_ek3223.py
+++ tests/test_cipher_ek3223.py
@@ -1,1 +1,20 @@
 from cipher_ek3223 import cipher_ek3223
+import pytest
+
+def test_cipher():
+    example = 'broski'
+    expected = 'csptlj'
+    actual = cipher_ek3223.cipher(example, 1)
+    assert actual == expected
+
+def test_backwards_cipher():
+    example = 'hubbabubba'
+    expected = 'gtaaZataaZ'
+    actual = cipher_ek3223.cipher(example, -1)
+    assert actual == expected
+
+def test_cipher_symbols():
+    example = '!@#$%^&* *&^%$#@!'
+    expected = '!@#$%^&* *&^%$#@!'
+    actual = cipher_ek3223.cipher(example, 10)
+    assert actual == expected
